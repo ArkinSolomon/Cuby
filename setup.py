18081464da49cb40470943fe9dc25a7771d86376
ArkinSolomon <arkinsolomon@gmail.com>
2020-02-15 15:55:32 -0800

    Unsure if this setup file will work

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from glob import glob
 import py2exe
 
 data_files = [('images', glob('images/*.*')), ('audio', glob('audio/*.*')), ('levels', glob('levels/*.*'))]
-setup(console=['__main__.py'], data_files=data_files)
+setup(console=['__main__.py'], data_files=data_files, name='Cuby', icon_resources=[(0, 'images/icon.ico')])
